9299c4340f67af670d4d8dfb2eaff1334e9f816e
enmanuelbayen <enmanuel0495@gmail.com>
2023-04-19 14:22:23 -0400

    Added speaker section cards  dynamically with JS

--- JS/main.js
+++ JS/main.js
@@ -1,3 +1,4 @@
+// menu popup
 const hamburge = document.querySelector('.hamburge');
 const menuBody = document.querySelector('.header');
 
@@ -28,3 +29,68 @@ hamburge.addEventListener ( 'click' , () => {
   });
 
 })
+
+// Spearker cards
+
+const speakers = [
+  {
+    photo: "./img/team-photo1.jpg",
+    name: "Melina Wolf",
+    profession: "Office Management",
+    resume: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
+  },
+  {
+    photo: "./img/team-photo2.png",
+    name: "Martine Schumacher",
+    profession: "Project & Speaker Manager",
+    resume: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
+  },
+  {
+    photo: "./img/team-photo3.jpg",
+    name: "Iryna Ivanova",
+    profession: "Sales Manager",
+    resume: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
+  },
+  {
+    photo: "./img/team-photo4.jpg",
+    name: "Stephan Reichart",
+    profession: "Head of devcom",
+    resume: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
+  },
+  {
+    photo: "./img/team-photo5.jpg",
+    name: "Tristan Schulze-boving",
+    profession: "Event Manager",
+    resume: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
+  },
+  {
+    photo: "./img/team-photo6.jpg",
+    name: "Nico Balletta",
+    profession: "Head of Program",
+    resume: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
+  }
+];
+
+const speakerBody = document.querySelector('.team-container');
+
+window.onload = () => {
+  for (let i = 0; i < speakers.length; i += 1){
+const deck = document.createElement('div');
+deck.className = "team-card grid"; 
+deck.innerHTML = `
+  <div class="team-img-box">
+    <img src="./img/chest.jpg" alt="chest table image" class="chestbg">
+    <img src="${speakers[i].photo}" alt="photograh of team members" class="team-photo">
+  </div>
+  <div class="team-info flex">
+    <h3 class="team-name">${speakers[i].name}</h3>
+    <p class="job-position">${speakers[i].profession}</p>
+    <span class="job-underline"></span>
+    <p class="team-resume">${speakers[i].resume}</p>
+  </div>
+`;
+
+speakerBody.appendChild(deck);
+  }
+ 
+}
